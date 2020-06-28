29822514e91fa2fe9499d6a6709a2d82e72e3dd5
rexsateesh <rex.sateesh@gmail.com>
2020-06-28 13:51:17 +0530

    park car implementation

--- car.js
+++ car.js
@@ -15,6 +15,32 @@ function Car() {
         this.slots = Array.from(Array(cap), (_, i) => i + 1);
         return `Created parking lot with ${cap} slots`;
     }
+
+    this.parkCar = (carNumber) => {
+        if (this.slots.length === 0) {
+            return 'Unable to park a car. Make sure parking is alloted?';
+        }
+
+        const parkedSlot = this.parking.filter((item) => item != null).map(i => i.slotNumber); // Get how many cars parked already in parking
+
+        // If parking full
+        if (parkedSlot.length === this.slots.length) {
+            return 'Sorry, parking lot is full';
+        }
+        
+        // Filter to check duplicate car entry
+        const checkCar = this.parking.filter(item => item != null && item.carNumber == carNumber).length;
+        if (checkCar) {
+            return `Duplicate entry for parked car ${carNumber}.`;
+        }
+
+        const availableSlots = this.slots.filter(s => !parkedSlot.includes(s)); // Find available slots
+        const slotNumber = availableSlots[0]; // Slot number
+
+        // Park a car
+        this.parking.push({carNumber, slotNumber});
+        return `Allocated slot number: ${slotNumber}`;
+    }
 }
 
 module.exports = Car;
