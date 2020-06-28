70418be3c18f68bb6c829915ab9dcc03cb5e082a
rexsateesh <rex.sateesh@gmail.com>
2020-06-28 13:51:00 +0530

    Create parking lot implementation

--- car.js
+++ car.js
@@ -2,7 +2,19 @@ function Car() {
     this.slots = [];
     this.parking = [];
     
-    
+    this.createParkingLot = (capacity) => {
+        if (typeof capacity === 'undefined') { // If capacity not provided
+            return 'Capacity is required';
+        }
+
+        const cap = parseInt(capacity); // Parse to integer
+        if (isNaN(cap)) {
+            return 'Capacity must be in number format';
+        }
+
+        this.slots = Array.from(Array(cap), (_, i) => i + 1);
+        return `Created parking lot with ${cap} slots`;
+    }
 }
 
 module.exports = Car;
